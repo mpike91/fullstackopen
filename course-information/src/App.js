9e014716a0a3b1260c6b69a0078018b46bf1f399
Matt Pike <mattp91@gmail.com>
2023-08-01 12:35:33 -0400

    Exercises partB

--- course-information/src/App.js
+++ course-information/src/App.js
@@ -1,44 +1,55 @@
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} exercise={exercises1} />
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
-      <Total exercises={exercises1 + exercises2 + exercises3} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
 
-const Header = ({ course, exercise }) => <h1>{course}</h1>;
+const Header = ({ course }) => <h1>{course}</h1>;
 
-const Content = (props) => (
+const Content = ({ parts }) => (
   <>
-    <Part part={props.part1} exercises={props.exercises1} />
-    <Part part={props.part2} exercises={props.exercises2} />
-    <Part part={props.part1} exercises={props.exercises3} />
+    {parts.map((part) => (
+      <Part name={part.name} exercises={part.exercises} />
+    ))}
   </>
 );
 
 const Part = (props) => (
   <p>
-    {props.part} {props.exercises}
+    {props.name} {props.exercises}
   </p>
 );
 
-const Total = ({ exercises }) => <p>Number of exercises {exercises}</p>;
+const Total = (props) => (
+  <p>
+    Number of exercises{" "}
+    {props.parts.reduce(
+      (total, currentElement) => total + currentElement.exercises,
+      0
+    )}
+  </p>
+);
 
 export default App;
