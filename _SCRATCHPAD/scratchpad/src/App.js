a9ccaefeef0c9c31fe313cd2c31fa2626cf17cc5
Matt Pike <mattp91@gmail.com>
2023-08-08 16:53:28 -0400

    Updated Notes functionality

--- _SCRATCHPAD/scratchpad/src/App.js
+++ _SCRATCHPAD/scratchpad/src/App.js
@@ -122,17 +122,50 @@
 // export default App;
 
 // PART 2
+import { useState, useSyncExternalStore } from "react";
 import Note from "./components/Note";
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("A new note...");
+  const [showAll, setShowAll] = useState(true);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+
+    setNotes([...notes, noteObject]);
+    setNewNote("");
+  };
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          Show {showAll ? "Important" : "All"}
+        </button>
+      </div>
       <ul>
-        {notes.map((note) => (
+        {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">Save</button>
+      </form>
     </div>
   );
 };
