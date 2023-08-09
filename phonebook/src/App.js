0727db75fd3e56599bef515b5472a028d557cc88
Matt Pike <mattp91@gmail.com>
2023-08-09 17:22:45 -0400

    Added json-server, refactored to interact the server using axios

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+
+import personService from "./services/persons";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -7,20 +8,44 @@ const App = () => {
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
 
-  const hook = () => {
-    axios.get("http://localhost:3001/persons").then((response) => {
-      console.log("promise fulfilled");
-      setPersons(response.data);
-    });
+  // Get all persons from server and set them to state
+  const getPersons = () => {
+    personService.getAll().then((allPersons) => setPersons(allPersons));
   };
 
-  useEffect(hook, []);
+  // Upon initial render, getPersons
+  useEffect(() => getPersons(), []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (persons.find((person) => person.name === newName))
-      return alert(`${newName} is already added to phonebook.`);
-    setPersons([...persons, { name: newName, number: newNumber }]);
+    // Check is person exists already
+    const person = persons.find(
+      (p) => p.name.toLowerCase() === newName.toLowerCase()
+    );
+
+    // If person is not in phonebook, create new person and update state.
+    if (!person) {
+      personService
+        .create({ name: newName, number: newNumber })
+        .then((returnedPerson) => setPersons([...persons, returnedPerson]));
+    }
+    // If person is already in phonebook, alert user before updating.
+    else if (
+      window.confirm(`${newName} is already added to phonebook. Update number?`)
+    ) {
+      // If person confirms, then update the user and then repopulate.
+      personService
+        .update(person.id, { name: newName, number: newNumber })
+        .then(() => getPersons());
+    }
+  };
+
+  const handleDelete = (id) => {
+    console.log(id);
+    // Alerts user before deletion, then deletes person from server if confirmed and repopulates data from server
+    if (window.confirm("Do you really want to delete?")) {
+      personService.deleteOne(id).then(() => getPersons());
+    }
   };
 
   return (
@@ -34,7 +59,7 @@ const App = () => {
         handleSubmit={handleSubmit}
       />
       <h2>Numbers</h2>
-      <Numbers persons={persons} filter={filter} />
+      <Numbers persons={persons} filter={filter} handleDelete={handleDelete} />
     </div>
   );
 };
@@ -59,19 +84,25 @@ const AddPerson = ({ setNewName, setNewNumber, handleSubmit }) => (
   </form>
 );
 
-const Numbers = ({ persons, filter }) => (
+const Numbers = ({ persons, filter, handleDelete }) => (
   <ul>
     {persons.map((person) => (
-      <Person key={person.name} person={person} filter={filter} />
+      <Person
+        key={person.id}
+        person={person}
+        filter={filter}
+        handleDelete={handleDelete}
+      />
     ))}
   </ul>
 );
 
-const Person = ({ person, filter }) => {
+const Person = ({ person, filter, handleDelete }) => {
   if (person.name.toLowerCase().includes(filter.toLowerCase())) {
     return (
       <li>
-        {person.name} ({person.number})
+        {person.name} ({person.number}){" "}
+        <button onClick={() => handleDelete(person.id)}>Delete</button>
       </li>
     );
   }
