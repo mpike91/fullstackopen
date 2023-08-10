0d0f3d24b8aa3244a4320b0cac073a90bba05251
Matt Pike <mattp91@gmail.com>
2023-08-10 12:29:08 -0400

    Added error message

--- notes/src/App.js
+++ notes/src/App.js
@@ -2,12 +2,15 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 import Note from "./components/Note";
+import Notification from "./components/Notification";
+
 import noteService from "./services/notes";
 
 const App = (props) => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("A new note...");
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     noteService.getAll().then((initialNotes) => {
@@ -25,7 +28,13 @@ const App = (props) => {
         setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
       })
       .catch((error) => {
-        alert(`the note '${note.content}' was already deleted from server`);
+        // alert(`the note '${note.content}' was already deleted from server`);
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server.`
+        );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
         setNotes(notes.filter((n) => n.id !== id));
       });
   };
@@ -53,6 +62,7 @@ const App = (props) => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           Show {showAll ? "Important" : "All"}
