f91b9212ac6ef94ce1f9bc84ab93ebd7fce4e041
Matt Pike <mattp91@gmail.com>
2023-08-25 13:12:11 -0400

    Setup backend for use with MongoDB

--- notes-node/index.js
+++ notes-node/index.js
@@ -1,33 +1,16 @@
+require("dotenv").config();
+
 const express = require("express");
+const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
-const mongoose = require("mongoose");
-const app = express();
+const Note = require("./models/note");
 
 // Middleware stack
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.static("build"));
-
-// Mongoose setup & connection
-if (process.argv.length < 3)
-  console.log(
-    "Mongoose not connected. Provide password, eg: 'npm run dev password'"
-  );
-
-const url = `mongodb+srv://fullstack:${process.argv[2]}@cluster0.zaec9je.mongodb.net/noteApp?retryWrites=true&w=majority`;
-mongoose.set("strictQuery", false);
-mongoose.connect(url);
-
-// Mongoose note schema
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-});
-
-// Mongoose Note model
-const Note = mongoose.model("Note", noteSchema);
 
 // Base URL
 app.get("/", (request, response) => {
@@ -41,24 +24,17 @@ app.get("/api/notes", (request, response) => {
 
 // Get node by id
 app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-  note ? response.json(note) : response.status(404).end();
+  Note.findById(request.params.id).then((note) => response.json(note));
 });
 
 // Delete note by id
 app.delete("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
-
-  response.status(204).end();
+  Note.findByIdAndDelete(request.params.id).then((note) =>
+    note
+      ? response.status(204).json({})
+      : response.status(400).json({ message: "No such id found" })
+  );
 });
-
-// Generate new id
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
 
 // Create new note
 app.post("/api/notes", (request, response) => {
@@ -70,19 +46,16 @@ app.post("/api/notes", (request, response) => {
     });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
-
-  response.json(note);
+  note.save().then((savedNote) => response.json(savedNote));
 });
 
 // Start server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
